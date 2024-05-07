8f56862d6929b0f5cec7c739e5756b5e320838e3
khaled-ahmed-mohamed <143337127+khaled-ahmed-mohamed@users.noreply.github.com>
2024-05-07 11:12:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidebar from "./componets/sidebar/sidebar";
+import Main from "./componets/main/main";
+ 
+ 
+
+const App = () => {
+  
+  return(
+    <>
+     <Sidebar/>
+     <Main/>
+    </>
+  )
+}
+
+export default App;
